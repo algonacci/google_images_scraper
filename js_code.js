7fb7bd20bbbd7cec71716a4b787d29ee23bf9977
Eric Julianto <algonacci1@gmail.com>
2023-03-22 15:23:22 +0700

    Update js_code.js

--- js_code.js
+++ js_code.js
@@ -1,60 +1,43 @@
-
-/*
- referensi: https://www.pyimagesearch.com/2017/12/04/how-to-create-a-deep-learning-dataset-using-google-images/
-
- Cara mendapatkan urls.txt
- 1. Buka google chrome image
- 2. Cari gambar yang ingin di download
- 3. Scroll sampai bawah
- 4. Buka dev tools chrome dengan cara, klik titik 3 di pojok kanan atas, lalu pilih more tools, dan klik
-    Develover tools
- 5. Pilih console
- 6. Lalu paste kode js dibawah ini
- 7. Tunggu sampai teks file urls.txt ter-downloads
-*/
-
-
 /**
  * simulate a right-click event so we can grab the image URL using the
  * context menu alleviating the need to navigate to another page
  *
- * attributed to @jmiserez: http://pyimg.co/9qe7y
  *
  * @param   {object}  element  DOM Element
  *
  * @return  {void}
  */
- function simulateRightClick( element ) {
-    var event1 = new MouseEvent( 'mousedown', {
-        bubbles: true,
-        cancelable: false,
-        view: window,
-        button: 2,
-        buttons: 2,
-        clientX: element.getBoundingClientRect().x,
-        clientY: element.getBoundingClientRect().y
-    } );
-    element.dispatchEvent( event1 );
-    var event2 = new MouseEvent( 'mouseup', {
-        bubbles: true,
-        cancelable: false,
-        view: window,
-        button: 2,
-        buttons: 0,
-        clientX: element.getBoundingClientRect().x,
-        clientY: element.getBoundingClientRect().y
-    } );
-    element.dispatchEvent( event2 );
-    var event3 = new MouseEvent( 'contextmenu', {
-        bubbles: true,
-        cancelable: false,
-        view: window,
-        button: 2,
-        buttons: 0,
-        clientX: element.getBoundingClientRect().x,
-        clientY: element.getBoundingClientRect().y
-    } );
-    element.dispatchEvent( event3 );
+function simulateRightClick(element) {
+  var event1 = new MouseEvent("mousedown", {
+    bubbles: true,
+    cancelable: false,
+    view: window,
+    button: 2,
+    buttons: 2,
+    clientX: element.getBoundingClientRect().x,
+    clientY: element.getBoundingClientRect().y,
+  });
+  element.dispatchEvent(event1);
+  var event2 = new MouseEvent("mouseup", {
+    bubbles: true,
+    cancelable: false,
+    view: window,
+    button: 2,
+    buttons: 0,
+    clientX: element.getBoundingClientRect().x,
+    clientY: element.getBoundingClientRect().y,
+  });
+  element.dispatchEvent(event2);
+  var event3 = new MouseEvent("contextmenu", {
+    bubbles: true,
+    cancelable: false,
+    view: window,
+    button: 2,
+    buttons: 0,
+    clientX: element.getBoundingClientRect().x,
+    clientY: element.getBoundingClientRect().y,
+  });
+  element.dispatchEvent(event3);
 }
 
 /**
@@ -66,15 +49,15 @@
  *
  * @return  {string}               value
  */
- function getURLParam( queryString, key ) {
-    var vars = queryString.replace( /^\?/, '' ).split( '&' );
-    for ( let i = 0; i < vars.length; i++ ) {
-        let pair = vars[ i ].split( '=' );
-        if ( pair[0] == key ) {
-            return pair[1];
-        }
+function getURLParam(queryString, key) {
+  var vars = queryString.replace(/^\?/, "").split("&");
+  for (let i = 0; i < vars.length; i++) {
+    let pair = vars[i].split("=");
+    if (pair[0] == key) {
+      return pair[1];
     }
-    return false;
+  }
+  return false;
 }
 
 /**
@@ -84,12 +67,12 @@
  *
  * @return  {void}
  */
- function createDownload( contents ) {
-    var hiddenElement = document.createElement( 'a' );
-    hiddenElement.href = 'data:attachment/text,' + encodeURI( contents );
-    hiddenElement.target = '_blank';
-    hiddenElement.download = 'urls.txt';
-    hiddenElement.click();
+function createDownload(contents) {
+  var hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:attachment/text," + encodeURI(contents);
+  hiddenElement.target = "_blank";
+  hiddenElement.download = "urls.txt";
+  hiddenElement.click();
 }
 
 /**
@@ -98,46 +81,48 @@
  *
  * @return  {object}  Promise object
  */
- function grabUrls() {
-    var urls = [];
-    return new Promise( function( resolve, reject ) {
-        var count = document.querySelectorAll(
-        	'.isv-r a:first-of-type' ).length,
-            index = 0;
-        Array.prototype.forEach.call( document.querySelectorAll(
-        	'.isv-r a:first-of-type' ), function( element ) {
-            // using the right click menu Google will generate the
-            // full-size URL; won't work in Internet Explorer
-            // (http://pyimg.co/byukr)
-            simulateRightClick( element.querySelector( ':scope img' ) );
-            // Wait for it to appear on the <a> element
-            var interval = setInterval( function() {
-                if ( element.href.trim() !== '' ) {
-                    clearInterval( interval );
-                    // extract the full-size version of the image
-                    let googleUrl = element.href.replace( /.*(\?)/, '$1' ),
-                        fullImageUrl = decodeURIComponent(
-                        	getURLParam( googleUrl, 'imgurl' ) );
-                    if ( fullImageUrl !== 'false' ) {
-                        urls.push( fullImageUrl );
-                    }
-                    // sometimes the URL returns a "false" string and
-                    // we still want to count those so our Promise
-                    // resolves
-                    index++;
-                    if ( index == ( count - 1 ) ) {
-                        resolve( urls );
-                    }
-                }
-            }, 10 );
-        } );
-    } );
+function grabUrls() {
+  var urls = [];
+  return new Promise(function (resolve, reject) {
+    var count = document.querySelectorAll(".isv-r a:first-of-type").length,
+      index = 0;
+    Array.prototype.forEach.call(
+      document.querySelectorAll(".isv-r a:first-of-type"),
+      function (element) {
+        // using the right click menu Google will generate the
+        // full-size URL; won't work in Internet Explorer
+        // (http://pyimg.co/byukr)
+        simulateRightClick(element.querySelector(":scope img"));
+        // Wait for it to appear on the <a> element
+        var interval = setInterval(function () {
+          if (element.href.trim() !== "") {
+            clearInterval(interval);
+            // extract the full-size version of the image
+            let googleUrl = element.href.replace(/.*(\?)/, "$1"),
+              fullImageUrl = decodeURIComponent(
+                getURLParam(googleUrl, "imgurl")
+              );
+            if (fullImageUrl !== "false") {
+              urls.push(fullImageUrl);
+            }
+            // sometimes the URL returns a "false" string and
+            // we still want to count those so our Promise
+            // resolves
+            index++;
+            if (index == count - 1) {
+              resolve(urls);
+            }
+          }
+        }, 10);
+      }
+    );
+  });
 }
 
 /**
  * Call the main function to grab the URLs and initiate the download
  */
- grabUrls().then( function( urls ) {
-    urls = urls.join( '\n' );
-    createDownload( urls );
-} );
+grabUrls().then(function (urls) {
+  urls = urls.join("\n");
+  createDownload(urls);
+});
